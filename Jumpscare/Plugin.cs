db2c7a2adba2a953d253104e3675a2d74325ed15
kenneth <kokiidoki@gmail.com>
2026-06-18 16:16:12 -0400

    chore: init

---
+++ Jumpscare/Plugin.cs
@@ -0,0 +1,35 @@
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using BepInEx;
+using BepInEx.Logging;
+using ComputerysModdingUtilities;
+using HarmonyLib;
+using HarmonyLib.Tools;
+using Jumpscare;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+[assembly: StraftatMod(isVanillaCompatible: true)]
+
+[BepInDependency(MyceliumNetworking.MyPluginInfo.PLUGIN_GUID)]
+[BepInPlugin(MyPluginInfo.PLUGIN_GUID, MyPluginInfo.PLUGIN_NAME, MyPluginInfo.PLUGIN_VERSION)]
+public class JumpscarePlugin : BaseUnityPlugin
+{
+    internal static JumpscarePlugin Instance;
+    Harmony _harmony = new(MyPluginInfo.PLUGIN_GUID);
+
+    void Awake()
+    {
+        this.gameObject.hideFlags = HideFlags.HideAndDontSave;
+        Instance = this;
+
+        _harmony.PatchAll();
+    }
+
+    void OnDestroy()
+    {
+        _harmony.UnpatchSelf();
+    }
+}
